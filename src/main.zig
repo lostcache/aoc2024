bfa98395ccb47fa6225b6dd929e798570e9456a0
Harsh Devisha <harshdevisha9111@gmail.com>
2024-12-08 19:06:54 -0800

    feat(sol) day5-8

--- src/main.zig
+++ src/main.zig
@@ -3,10 +3,18 @@ const day1 = @import("sol/day1.zig");
 const day2 = @import("sol/day2.zig");
 const day3 = @import("sol/day3.zig");
 const day4 = @import("sol/day4.zig");
+const day5 = @import("sol/day5.zig");
+const day6 = @import("sol/day6.zig");
+const day7 = @import("sol/day7.zig");
+const day8 = @import("sol/day8.zig");
 
 pub fn main() !void {
     // _ = try day1.day1();
     // try day2.day2();
     // try day3.day3();
     // try day4.day4();
+    // try day5.day5();
+    // try day6.day6();
+    // try day7.day7();
+    try day8.day8();
 }
