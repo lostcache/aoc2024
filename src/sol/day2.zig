4f58aa79541a6934c165eae871cfc0d74704af67
Harsh Devisha <harshdevisha9111@gmail.com>
2024-12-12 12:51:12 -0800

    feat(sol) day2

--- src/sol/day2.zig
+++ src/sol/day2.zig
@@ -7,32 +7,20 @@ const Ord = enum {
     Dunno,
 };
 
-fn isWithinTollerance(line: []const u8, tollerance: i32) !bool {
-    var faults: i32 = 0;
-    var levels = std.mem.split(u8, line, " ");
-    const maybeFirstLevel = levels.next();
-    if (maybeFirstLevel == null) return false;
-
-    var prevLevel = try std.fmt.parseInt(i64, maybeFirstLevel.?, 10);
+fn isWithinTollerance(levelArr: *std.ArrayList(i64)) !bool {
+    var prevLevel = levelArr.items[0];
 
     var ord: Ord = Ord.Dunno;
-    var ordChangeCounter: u32 = 0;
 
-    while (levels.next()) |level| {
-        if (faults > tollerance) return false;
-
-        const currLevel = try std.fmt.parseInt(i64, level, 10);
-
+    for (levelArr.items[1..]) |currLevel| {
         const diff = currLevel - prevLevel;
 
         if (diff == 0) {
-            faults += 1;
-            continue;
+            return false;
         }
 
         if (diff > 3 or diff < -3) {
-            faults += 1;
-            continue;
+            return false;
         }
 
         switch (ord) {
@@ -45,32 +33,18 @@ fn isWithinTollerance(line: []const u8, tollerance: i32) !bool {
             },
             .Asc => {
                 if (diff < 0) {
-                    ord = Ord.Desc;
-                    ordChangeCounter += 1;
-                    if (ordChangeCounter == 2) {
-                        ordChangeCounter = 0;
-                        faults += 1;
-                        continue;
-                    }
+                    return false;
                 }
             },
             .Desc => {
                 if (diff > 0) {
-                    ord = Ord.Asc;
-                    ordChangeCounter += 1;
-                    if (ordChangeCounter == 2) {
-                        ordChangeCounter = 0;
-                        faults += 1;
-                        continue;
-                    }
+                    return false;
                 }
             },
         }
 
         prevLevel = currLevel;
     }
-
-    if (faults > tollerance) return false;
 
     return true;
 }
@@ -90,13 +64,107 @@ pub fn day2() !void {
     while (lines.next()) |line| {
         if (line.len == 0) continue;
 
-        const isWithinTolleranceForPart1 = try isWithinTollerance(line, 0);
-        if (isWithinTolleranceForPart1) part1 += 1;
+        var levelsArr = std.ArrayList(i64).init(alloc);
+        var levels = std.mem.split(u8, line, " ");
+        while (levels.next()) |level| {
+            const persedLevel = try std.fmt.parseInt(i64, level, 10);
+            _ = try levelsArr.append(persedLevel);
+        }
 
-        const isWithinTolleranceForPart2 = try isWithinTollerance(line, 1);
-        if (isWithinTolleranceForPart2) part2 += 1;
+        if (levelsArr.items.len < 2) {
+            part1 += 1;
+            part2 += 1;
+            continue;
+        }
+
+        const isWithinTolleranceForPart1 = try isWithinTollerance(&levelsArr);
+        if (isWithinTolleranceForPart1) {
+            part1 += 1;
+            part2 += 1;
+        } else {
+            for (0..levelsArr.items.len) |index| {
+                var filteredArr = std.ArrayList(i64).init(alloc);
+                for (0..levelsArr.items.len) |i| {
+                    if (i == index) continue;
+                    _ = try filteredArr.append(levelsArr.items[i]);
+                }
+                const isWithinTolleranceForPart2 = try isWithinTollerance(&filteredArr);
+                if (isWithinTolleranceForPart2) {
+                    part2 += 1;
+                    break;
+                }
+            }
+        }
+
+        // const isWithinTolleranceForPart2 = try isWithinTollerance(line, 1);
+        // if (isWithinTolleranceForPart2) part2 += 1;
+
+        // var faults: i32 = 0;
+
+        // var left: usize = 0;
+        // var right: usize = 1;
+        // var order: Ord = Ord.Dunno;
+
+        // while (right < levelsArr.items.len) {
+        //     const leftVal = levelsArr.items[left];
+        //     const rightVal = levelsArr.items[right];
+
+        //     if (order == Ord.Dunno) {
+        //         if (leftVal > rightVal) {
+        //             order = Ord.Desc;
+        //         } else if (leftVal < rightVal) {
+        //             order = Ord.Asc;
+        //         } else {
+        //             if (left > 0) {
+        //                 left -= 1;
+        //             } else {
+        //                 right += 1;
+        //             }
+        //             faults += 1;
+        //             continue;
+        //         }
+        //     }
+
+        //     if (leftVal - rightVal > 3 or leftVal - rightVal < -3) {
+        //         if (left > 0) {
+        //             left -= 1;
+        //         } else {
+        //             right += 1;
+        //         }
+        //         order = Ord.Dunno;
+        //         faults += 1;
+        //         continue;
+        //     }
+
+        //     if (leftVal > rightVal and order == Ord.Asc) {
+        //         if (left > 0) {
+        //             left -= 1;
+        //         } else {
+        //             right += 1;
+        //         }
+        //         order = Ord.Dunno;
+        //         faults += 1;
+        //         continue;
+        //     }
+
+        //     if (leftVal < rightVal and order == Ord.Desc) {
+        //         if (left > 0) {
+        //             left -= 1;
+        //         } else {
+        //             right += 1;
+        //         }
+        //         order = Ord.Dunno;
+        //         faults += 1;
+        //         continue;
+        //     }
+
+        //     left += 1;
+        //     right += 1;
+        // }
+
     }
 
     std.debug.print("Part 1: {}\n", .{part1});
+
     std.debug.print("Part 2: {}\n", .{part2});
 }
