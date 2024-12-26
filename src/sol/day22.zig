865e04b8d9f538fc5aa988b5e98cfea03d0097ec
Harsh Devisha <harshdevisha9111@gmail.com>
2024-12-25 23:41:51 -0800

    refactor(sol) day-22 part-2 WIP

--- src/sol/day22.zig
+++ src/sol/day22.zig
@@ -1,34 +1,41 @@
 const std = @import("std");
 const utils = @import("../utils.zig");
 
-fn parseInput(buffer: *[]const u8, nums: *std.ArrayList(i8)) !void {
+fn parseInput(buffer: *[]const u8, nums: *std.ArrayList(i64)) !void {
     var numIter = std.mem.split(u8, buffer.*, "\n");
 
     while (numIter.next()) |numStr| {
         if (numStr.len == 0) continue;
-        try nums.append(try std.fmt.parseInt(i8, numStr, 10));
+        try nums.append(try std.fmt.parseInt(i64, numStr, 10));
     }
 }
 
-fn getSecret(num: i8) i8 {
+fn getSecret(num: i64) i64 {
     var initialNum = num;
 
     initialNum = @mod(((initialNum * 64) ^ initialNum), 16777216);
 
-    initialNum = @mod(((initialNum / 32) ^ initialNum), 16777216);
+    initialNum = @mod((@divTrunc(initialNum, 32) ^ initialNum), 16777216);
 
     initialNum = @mod(((initialNum * 2048) ^ initialNum), 16777216);
 
     return initialNum;
 }
 
-fn processData(num: i8, alloc: *std.mem.Allocator) !void {
-    var diffArr = try std.ArrayList(i8).initCapacity(alloc.*, 2000);
+fn processData(num: i64, diffArr: *std.ArrayList(i8), seqMap: *std.AutoHashMap([4]i8, usize)) !void {
     var currNum = num;
-    for (0..2000) |_| {
+    var currNumOnesDigit = @mod(currNum, 10);
+    for (0..2000) |i| {
         const secret = getSecret(currNum);
-        diffArr.append(secret - currNum);
+        const secretOnesDigit = @mod(secret, 10);
+        try diffArr.append(@as(i8, @intCast(secretOnesDigit - currNumOnesDigit)));
         currNum = secret;
+        currNumOnesDigit = secretOnesDigit;
+
+        if (@mod(i + 1, 4) == 0) {
+            const seq = .{ diffArr.items[i - 3], diffArr.items[i - 2], diffArr.items[i - 1], diffArr.items[i] };
+            try seqMap.put(seq, i);
+        }
     }
 }
 
@@ -40,18 +47,17 @@ pub fn day22() !void {
     const path = "./src/inputs/day22.txt";
 
     var buffer = try utils.readFile(path, &alloc);
-    var nums = std.ArrayList(i8).init(alloc);
+    var nums = std.ArrayList(i64).init(alloc);
 
     try parseInput(&buffer, &nums);
 
-    var part1: usize = 0;
+    var diffData = try std.ArrayList(std.ArrayList(i8)).initCapacity(alloc, 1800);
+    var seqMaps = try std.ArrayList(std.AutoHashMap([4]i8, usize)).initCapacity(alloc, 1800);
     for (nums.items) |num| {
-        part1 += getFinalSecret(num);
-    }
-
-    std.debug.print("Part1: {}\n", .{part1});
-
-    for (nums.items) |num| {
-        try processData(num, &alloc);
+        var diffArr = try std.ArrayList(i8).initCapacity(alloc, 2000);
+        var seqMap = std.AutoHashMap([4]i8, usize).init(alloc);
+        try processData(num, &diffArr, &seqMap);
+        try diffData.append(diffArr);
+        try seqMaps.append(seqMap);
     }
 }
