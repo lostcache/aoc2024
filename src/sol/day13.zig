11ef338a63729bc5d2f3873574e5c3d376f9fe9f
Harsh Devisha <harshdevisha9111@gmail.com>
2024-12-26 21:33:02 -0800

    feat(sol) day 13

--- src/sol/day13.zig
+++ src/sol/day13.zig
@@ -145,6 +145,82 @@ fn getLeastCost(
     return minCost;
 }
 
+fn compareFn(_: void, a: [3]usize, b: [3]usize) std.math.Order {
+    if (a[2] < b[2]) return .lt;
+    if (a[2] > b[2]) return .gt;
+    return .eq;
+}
+
+fn getLeastCostDikstras(
+    aButtonVal: [2]usize,
+    bButtonVal: [2]usize,
+    target: [2]usize,
+    alloc: *std.mem.Allocator,
+) !usize {
+    var cache = std.AutoHashMap([2]usize, void).init(alloc.*);
+    var minHeap = std.PriorityQueue([3]usize, void, compareFn).init(alloc.*, {});
+    _ = try minHeap.add(.{ 0, 0, 0 });
+
+    while (minHeap.removeOrNull()) |state| {
+        const currX = state[0];
+        const currY = state[1];
+        const currrCost = state[2];
+
+        const maybeVisited = cache.get(.{ currX, currY });
+        if (maybeVisited != null) continue;
+
+        if (currX == target[0] and currY == target[1]) {
+            return currrCost;
+        }
+
+        if (currX > target[0] or currY > target[1]) {
+            continue;
+        }
+
+        _ = try minHeap.add(.{ currX + aButtonVal[0], currY + aButtonVal[1], currrCost + 3 });
+
+        _ = try minHeap.add(.{ currX + bButtonVal[0], currY + bButtonVal[1], currrCost + 1 });
+
+        _ = try cache.put(.{ currX, currY }, {});
+    }
+
+    return maxUsizeValue;
+}
+
+fn solveEquations(aButtonVal: [2]usize, bButtonVal: [2]usize, prizeVal: [2]usize) usize {
+    const ax = aButtonVal[0];
+    const ay = aButtonVal[1];
+    var bx = bButtonVal[0];
+    var by = bButtonVal[1];
+    var xx = prizeVal[0];
+    var yy = prizeVal[1];
+
+    bx = bx * ay;
+    xx = xx * ay;
+
+    by = by * ax;
+    yy = yy * ax;
+
+    var bn: usize = undefined;
+    if (bx > by) {
+        if (yy > xx) return 0;
+        bn = (xx - yy) / (bx - by);
+    } else {
+        if (xx > yy) return 0;
+        bn = (yy - xx) / (by - bx);
+    }
+
+    if (bn * bButtonVal[0] > prizeVal[0]) return 0;
+
+    const an: usize = (prizeVal[0] - (bn * bButtonVal[0])) / ax;
+
+    if ((an * aButtonVal[0] + bn * bButtonVal[0]) == prizeVal[0] and (an * aButtonVal[1] + bn * bButtonVal[1]) == prizeVal[1]) {
+        return an * 3 + bn * 1;
+    }
+
+    return 0;
+}
+
 pub fn day13() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
@@ -153,31 +229,43 @@ pub fn day13() !void {
     const path = "./src/inputs/day13.txt";
     var aButtonVals: ValueList = std.ArrayList([2]usize).init(alloc);
     var bButtonVals: ValueList = std.ArrayList([2]usize).init(alloc);
-    var PrizeVals: ValueList = std.ArrayList([2]usize).init(alloc);
+    var prizeVals: ValueList = std.ArrayList([2]usize).init(alloc);
 
     const buffer = try utils.readFile(path, &alloc);
 
-    try parseInputs(buffer, &aButtonVals, &bButtonVals, &PrizeVals);
+    try parseInputs(buffer, &aButtonVals, &bButtonVals, &prizeVals);
 
     const numMachines = aButtonVals.items.len;
 
     var totalCost: usize = 0;
+    // for (0..numMachines) |machineIndex| {
+    //     const machineCost = solveEquations(
+    //         aButtonVals.items[machineIndex],
+    //         bButtonVals.items[machineIndex],
+    //         prizeVals.items[machineIndex],
+    //     );
+
+    //     totalCost += machineCost;
+    // }
+
+    // std.debug.print("Part1: {}\n", .{totalCost});
+
+    for (0..prizeVals.items.len) |i| {
+        prizeVals.items[i][0] = prizeVals.items[i][0] + 10000000000000;
+        prizeVals.items[i][1] = prizeVals.items[i][1] + 10000000000000;
+    }
+
+    totalCost = 0;
     for (0..numMachines) |machineIndex| {
-        var cache = std.AutoHashMap([2]usize, usize).init(alloc);
-        const machineCost = try getLeastCost(
+        std.debug.print("Machine: {}\n", .{machineIndex});
+        const machineCost = solveEquations(
             aButtonVals.items[machineIndex],
             bButtonVals.items[machineIndex],
-            0,
-            0,
-            .{ 0, 0 },
-            PrizeVals.items[machineIndex],
-            &cache,
+            prizeVals.items[machineIndex],
         );
-        if (machineCost == maxUsizeValue) {
-            continue;
-        }
+
         totalCost += machineCost;
     }
 
-    std.debug.print("Total cost: {}\n", .{totalCost});
+    std.debug.print("Part2; {}\n", .{totalCost});
 }
